2f9290a21c676a799fb735e07f9f45a16b477ea6
Dylan Palmboom <nofxboy1234@gmail.com>
2023-12-28 15:00:52 +0200

    Move some functions into home.js module

---
+++ src/home.js
@@ -0,0 +1,18 @@
+const addWelcome = (parent, messages) => {
+  messages.forEach((text) => {
+    const div = document.createElement('div');
+    div.classList.add('welcome');
+    div.textContent = text;
+    parent.appendChild(div);
+  });
+};
+
+const messages = () => {
+  const messages = [];
+  messages.push('Welcome to RESTAURANT.');
+  messages.push("We've been around since the beginning of time.");
+  messages.push('We look forward to seeing you soon.');
+  return messages;
+};
+
+export { addWelcome, messages };
--- src/initial.js
+++ src/initial.js
@@ -1,26 +1,11 @@
+import { addWelcome, messages } from './home.js';
+
 const addLogo = (parent) => {
   const img = document.createElement('img');
   img.src = 'restaurant.png';
   img.alt = 'restaurant logo';
   img.id = 'logo';
   parent.appendChild(img);
-};
-
-const addWelcome = (parent, messages) => {
-  messages.forEach((text) => {
-    const div = document.createElement('div');
-    div.classList.add('welcome');
-    div.textContent = text;
-    parent.appendChild(div);
-  });
-};
-
-const messages = () => {
-  const messages = [];
-  messages.push('Welcome to RESTAURANT.');
-  messages.push("We've been around since the beginning of time.");
-  messages.push('We look forward to seeing you soon.');
-  return messages;
 };
 
 const initialPageLoad = () => {
